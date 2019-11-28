536b51a8f931d22bb8c89eb9b9a5b545474d8195
EC2 Default User <ec2-user@ip-172-31-33-214.us-east-2.compute.internal>
2019-11-28 15:32:41 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/new'
+
+  root 'static_pages#top'
+  
+  # ログイン機能
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
+  # ユーザー機能
+  resources :users
+
 end
